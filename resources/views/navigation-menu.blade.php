4a29b75d2828e093131a89b36cf89790df0fb3e2
Tenzin <dhontenzin@gmail.com>
2022-02-19 22:22:51 +0100

    admin image display link

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -92,7 +92,7 @@
                         <x-slot name="trigger">
                             @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                                 <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ asset('/storage/'.Auth::user()->profile_photo_path)}}" alt="{{ Auth::user()->name }}" />
                                 </button>
                             @else
                                 <span class="inline-flex rounded-md">
